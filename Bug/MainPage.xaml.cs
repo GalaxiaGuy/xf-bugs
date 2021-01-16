e6a3d3148dc64f2d86c4953f4180af1b11a67ca8
Oliver Brown <galaxiaguy@gmail.com>
2021-01-16 13:41:49 +0000

    Weird binding tests.

--- Bug/MainPage.xaml.cs
+++ Bug/MainPage.xaml.cs
@@ -9,7 +9,24 @@ namespace Bug
         {
             InitializeComponent();
             Span = 3;
+            HeaderColor = Color.Red;
             Items = new List<string> { "A", "B", "C", "D", "E", "F" };
+            Indirect.Span = 3;
+            Indirect.HeaderColor = Color.Red;
+            Indirect.Items = new List<string> { "A", "B", "C", "D", "E", "F" };
+        }
+
+        public PageProperties Indirect { get; } = new PageProperties();
+
+        private Color _headerColor = Color.Blue;
+        public Color HeaderColor
+        {
+            get => _headerColor;
+            set
+            {
+                _headerColor = value;
+                OnPropertyChanged();
+            }
         }
 
         private int _span = 1;
@@ -23,7 +40,43 @@ namespace Bug
             }
         }
 
-        private List<string> _items;
+        private List<string> _items = new List<string> { "X", "Y", "Z" };
+        public List<string> Items
+        {
+            get => _items;
+            set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public class PageProperties : BindableObject
+    {
+        private Color _headerColor = Color.Blue;
+        public Color HeaderColor
+        {
+            get => _headerColor;
+            set
+            {
+                _headerColor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _span = 1;
+        public int Span
+        {
+            get => _span;
+            set
+            {
+                _span = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<string> _items = new List<string> { "X", "Y", "Z" };
         public List<string> Items
         {
             get => _items;
