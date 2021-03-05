5a62f11dbb43a82dc79d610138f54a1166ae0576
Oliver Brown <galaxiaguy@gmail.com>
2021-03-05 12:25:16 +0000

    Bug.

--- Bug/MainPage.xaml.cs
+++ Bug/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace Bug
@@ -8,11 +10,12 @@ namespace Bug
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
         }
+    }
 
-        private async void CollectionViewGridPageButton_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new CollectionViewGridPage());
-        }
+    public class MainViewModel
+    {
+        public List<string> Labels { get; } = Enumerable.Repeat(".", 100).ToList();
     }
 }
