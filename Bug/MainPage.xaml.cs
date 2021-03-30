16df7021a059f9e769a1e76b52944876a28efad6
Oliver Brown <galaxiaguy@gmail.com>
2021-03-30 08:40:53 +0100

    Recreate bug

--- Bug/MainPage.xaml.cs
+++ Bug/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Bug
@@ -8,11 +13,40 @@ namespace Bug
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            Colors = new List<Color>
+            {
+                Color.Blue,
+                Color.Red,
+                Color.Green,
+                Color.Yellow,
+                Color.Purple,
+                Color.Magenta,
+                Color.Gray,
+                Color.Orange,
+            };
         }
 
-        private async void CollectionViewGridPageButton_Clicked(object sender, EventArgs e)
+        public Color CurrentColor { get; private set; }
+
+        public ICommand TapCommand => new AsyncCommand<Color>(async x =>
         {
-            await Navigation.PushAsync(new CollectionViewGridPage());
-        }
+            CurrentColor = x;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentColor)));
+            await Task.Delay(1000);
+            var options = new BrowserLaunchOptions { LaunchMode = BrowserLaunchMode.SystemPreferred, PreferredToolbarColor = CurrentColor };
+            await Browser.OpenAsync("https://www.github.com/xamarin/xamarin.forms", options);
+        });
+
+        public List<Color> Colors { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
