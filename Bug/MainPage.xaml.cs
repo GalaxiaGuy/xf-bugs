d781d8ecd6463ad9193b3c7df65e303794348501
Oliver Brown <galaxiaguy@gmail.com>
2021-04-22 14:33:35 +0100

    Test binding grid.

--- Bug/MainPage.xaml.cs
+++ Bug/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace Bug
@@ -8,12 +9,36 @@ namespace Bug
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = new MainViewModel();
+            BindingContext = new MainViewModel
+            {
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        Text = "A", X = 0, Y = 0
+                    },
+                    new Item
+                    {
+                        Text = "B", X = 1, Y = 0
+                    },
+                    new Item
+                    {
+                        Text = "C", X = 2, Y = 2
+                    }
+                }
+            };
         }
     }
 
     public class MainViewModel
     {
+        public List<Item> Items { get; set; }
+    }
 
+    public class Item
+    {
+        public string Text { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
