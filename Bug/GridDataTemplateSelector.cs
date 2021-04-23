25d3fb95e66d3eeb6af39b5997f4d4f6af47a21a
Oliver Brown <galaxiaguy@gmail.com>
2021-04-23 08:15:27 +0100

    Blah.

---
+++ Bug/GridDataTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TestBinadleLayout
+{
+    public class GridDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CountryFlagTemplate { get; set; }
+        public DataTemplate UserNameTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var data = (GridData)item;
+            if (!string.IsNullOrEmpty(data.Image))
+            {
+                return CountryFlagTemplate;
+            }
+            return UserNameTemplate;
+        }
+    }
+
+    public class GridData
+    {
+        public int Column { get; set; }
+        public int Row { get; set; }
+        public string Image { get; set; }
+        public string Text { get; set; }
+    }
+}
+
--- Bug/MainPage.xaml.cs
+++ Bug/MainPage.xaml.cs
@@ -1,44 +1,96 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace Bug
+namespace TestBinadleLayout
 {
     public partial class MainPage : ContentPage
     {
+        public class CreditData
+        {
+            public string user_id { get; set; }
+            public string user_name { get; set; }
+            public string country_code { get; set; }
+
+            public string country_flag
+            {
+                get
+                {
+                    return string.Format("https://www.jassimrahma.net/images/flags/{0}.png", country_code);
+                }
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = new MainViewModel
-            {
-                Items = new List<Item>
-                {
-                    new Item
-                    {
-                        Text = "A", X = 0, Y = 0
-                    },
-                    new Item
-                    {
-                        Text = "B", X = 1, Y = 0
-                    },
-                    new Item
-                    {
-                        Text = "C", X = 2, Y = 2
-                    }
-                }
-            };
+
+            PopulateCredits();
         }
-    }
 
-    public class MainViewModel
-    {
-        public List<Item> Items { get; set; }
-    }
+        public async void PopulateCredits()
+        {
+            try
+            {
+                var client = new HttpClient();
 
-    public class Item
-    {
-        public string Text { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
+                client.BaseAddress = new Uri("https://www.jassimrahma.net/populate_test_credits.php");
+
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("language", "EN"),
+                });
+
+                var response = await client.PostAsync("https://www.jassimrahma.net/populate_test_credits.php", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    List<CreditData> data = JsonSerializer.Deserialize<List<CreditData>>(result);
+
+                    BindableLayout.SetItemsSource(GridCredits, CreateGridcollection(data));
+
+                    ListViewCredits.ItemsSource = data;
+                    ListViewCredits.IsVisible = true;
+
+                    // viewModel.IsLoaded = true;
+                }
+                else
+                {
+                    await App.Current.MainPage.DisplayAlert("Error", "Error", "Error");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+
+                return;
+            }
+        }
+
+        private IEnumerable<GridData> CreateGridcollection(IEnumerable<CreditData> data)
+        {
+            var row = 0;
+            foreach (var item in data)
+            {
+                yield return new GridData { Row = row, Column = 0, Image = item.country_flag };
+                yield return new GridData { Row = row, Column = 1, Text = item.user_name };
+                row++;
+            }
+        }
     }
 }
