3d65fff367daa78595b7e090791b51a733482a7e
Oliver Brown <galaxiaguy@gmail.com>
2021-06-02 08:41:43 +0100

    Some test code.

--- Bug.iOS/AppDelegate.cs
+++ Bug.iOS/AppDelegate.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading.Tasks;
 using Foundation;
 using UIKit;
 
@@ -13,6 +13,8 @@ namespace Bug.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        private MyViewController _viewController;
+
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -25,7 +27,71 @@ namespace Bug.iOS
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
+            Test();
+
             return base.FinishedLaunching(app, options);
+        }
+
+        private async void Test()
+        {
+            await ExecuteAsync("Create UIViewController", () => new UIViewController());
+            await ExecuteAsync("Create UIView", () => new UIView());
+            await ExecuteAsync("Create UIColor", () => new UIColor(1, 1));
+
+            await ExecuteMyViewControllerAsync("Check if View is equal to null", x => x.CheckIfViewEqualsNull());
+            await ExecuteMyViewControllerAsync("Check if View is null pattern", x => x.CheckIfViewIsNull());
+            await ExecuteMyViewControllerAsync("Check if View is loaded", x => x.CheckIfViewIsLoaded());
+
+            await ExecuteMyViewControllerAsync("Check if View is equal to null after creating View", x => x.CheckIfViewEqualsNull(), x => x.CreateView());
+            await ExecuteMyViewControllerAsync("Check if View is null pattern after creating View", x => x.CheckIfViewIsNull(), x => x.CreateView());
+            await ExecuteMyViewControllerAsync("Check if View is loaded creating View", x => x.CheckIfViewIsLoaded(), x => x.CreateView());
+
+            await ExecuteMyViewControllerAsync("Check if non-exported View is equal to null", x => x.CheckIfNonExportedViewEqualsNull());
+            await ExecuteMyViewControllerAsync("Check if non-exported View is null pattern", x => x.CheckIfNonExportedViewIsNull());
+            await ExecuteMyViewControllerAsync("Check if exported View is equal to null", x => x.CheckIfExportedViewEqualsNull());
+            await ExecuteMyViewControllerAsync("Check if exported View is null pattern", x => x.CheckIfExportedViewIsNull());
+        }
+
+        private async Task ExecuteAsync(string name, Action action)
+        {
+            Console.WriteLine(name);
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    action();
+                    Console.WriteLine("OK");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception");
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine();
+            });
+        }
+
+        private async Task ExecuteMyViewControllerAsync(string name, Action<MyViewController> action, Action<MyViewController> setup = null)
+        {
+            Console.WriteLine(name);
+            _viewController = new MyViewController();
+            setup?.Invoke(_viewController);
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    action(_viewController);
+                    Console.WriteLine("OK");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception");
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine();
+            });
         }
     }
 }
---
+++ Bug.iOS/MyViewController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace Bug.iOS
+{
+    public class MyViewController : UIViewController
+    {
+        public MyViewController() : base()
+        {
+        }
+
+        public UIView NonExportedView { get; set; }
+
+        [Export("exportedView")]
+        public UIView ExportedView { get; set; }
+
+        public void CreateView()
+        {
+            _ = View;
+        }
+
+        public bool CheckIfViewEqualsNull()
+        {
+            return View == null;
+        }
+
+        public bool CheckIfViewIsNull()
+        {
+            return View is null;
+        }
+
+        public bool CheckIfNonExportedViewEqualsNull()
+        {
+            return NonExportedView == null;
+        }
+
+        public bool CheckIfNonExportedViewIsNull()
+        {
+            return NonExportedView is null;
+        }
+
+        public bool CheckIfExportedViewEqualsNull()
+        {
+            return ExportedView == null;
+        }
+
+        public bool CheckIfExportedViewIsNull()
+        {
+            return ExportedView is null;
+        }
+
+        public bool CheckIfViewIsLoaded()
+        {
+            return this.IsViewLoaded;
+        }
+    }
+}
