057b09416b833612394e632aab247beebb704ff4
Oliver Brown <oliver_brown2@next.co.uk>
2021-11-10 09:50:31 +0000

    Try workflow.

--- Bug.Android/Resources/Resource.designer.cs
+++ Bug.Android/Resources/Resource.designer.cs
@@ -14513,6 +14513,9 @@ namespace Bug.Droid
 			// aapt resource value: 0x7F07008F
 			public const int tooltip_frame_light = 2131165327;
 			
+			// aapt resource value: 0x7F070090
+			public const int xamarin = 2131165328;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
