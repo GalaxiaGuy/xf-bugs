d893d5f56f2c9274bf50def38a0e2f1dbb532bcc
Oliver Brown <oliver_brown2@next.co.uk>
2022-03-15 14:58:55 +0000

    Add carouselview-in-collectionview bug.

--- Bug/MainPage.xaml.cs
+++ Bug/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace Bug
@@ -8,6 +10,29 @@ namespace Bug
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+
+    public class MainViewModel
+    {
+        public List<Item> Items { get; }
+
+        public MainViewModel()
+        {
+            Items = Enumerable.Range(0, 30).Select(x => new Item()).ToList();
+        }
+    }
+
+    public class Item
+    {
+        private static readonly Random s_rand = new Random();
+
+        public List<Color> Colors { get; }
+
+        public Item()
+        {
+            Colors = Enumerable.Range(0, 3).Select(x => new Color(s_rand.NextDouble(), s_rand.NextDouble(), s_rand.NextDouble())).ToList();
         }
     }
 }
